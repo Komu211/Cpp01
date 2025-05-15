f2cba2ecf46a470f693dda1edf4c1257ee9e6c50
Komu211 <75761278+Komu211@users.noreply.github.com>
2025-05-15 11:44:30 +0200

    Done with 06

---
+++ ex06/Harl.cpp
@@ -0,0 +1,67 @@
+#include "Harl.hpp"
+#include <iostream>
+
+Harl::Harl()
+{
+}
+
+Harl::~Harl()
+{
+}
+
+void Harl::complain(std::string level)
+{
+	_logLevels logLevel = stringToLogLevel(level);
+
+	switch (logLevel)
+	{
+		case DEBUG:
+			debug();
+			return;
+		case INFO:
+			info();
+			return;
+		case WARNING:
+			warning();
+			return;
+		case ERROR:
+			error();
+			return;
+		default:
+			std::cout << "[ INVALID LEVEL ]" << std::endl;
+	}
+}
+
+Harl::_logLevels Harl::stringToLogLevel(std::string level)
+{
+	if (level == "DEBUG")
+		return DEBUG;
+	else if (level == "INFO")
+		return INFO;
+	else if (level == "WARNING")
+		return WARNING;
+	else if (level == "ERROR")
+		return ERROR;
+	else
+		return UNKNOWN;
+}
+
+void Harl::debug()
+{
+	std::cout << "[DEBUG] I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
+}
+
+void Harl::info()
+{
+	std::cout << "[INFO] I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!" << std::endl;
+}
+
+void Harl::warning()
+{
+	std::cout << "[WARNING] I think I deserve to have some extra bacon for free. I've been coming for years, whereas you started working here just last month." << std::endl;
+}
+
+void Harl::error()
+{
+	std::cout << "[ERROR] This is unacceptable! I want to speak to the manager now." << std::endl;
+}
---
+++ ex06/Harl.hpp
@@ -0,0 +1,29 @@
+#ifndef HARL_HPP
+#define HARL_HPP
+
+#include <string>
+
+class Harl
+{
+	public:
+		Harl();
+		~Harl();
+		void complain(std::string level);
+	private:
+		void debug(void);
+		void info(void);
+		void warning(void);
+		void error(void);
+		enum _logLevels
+		{
+			DEBUG,
+			INFO,
+			WARNING,
+			ERROR,
+			UNKNOWN
+		};
+		_logLevels stringToLogLevel(std::string level);
+
+};
+
+#endif
